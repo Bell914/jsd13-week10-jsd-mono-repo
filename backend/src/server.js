b15e135bc9630090aa49127733e7a2cc385a60c7
Bell914 <ittikorn.tipson@gmail.com>
2026-09-03 11:20:20 +0700

    add user API routes and protect environment file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,23 @@
 import express from "express";
+import { users } from "./fakeDB/fakeUsers.js";
 
 const app = express();
+
+// CRUD routes and endpoints
+
+// Read users
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// Create user
+app.post("/users", (req, res) => {});
+
+// Update user
+app.put("/users/:id", (req, res) => {});
+
+// Delete user
+app.delete("/users/:id", (req, res) => {});
 
 const PORT = 3001;
 
