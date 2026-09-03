dfd2722f3a36038e55f27465f6aa0ba8deb6a31a
Bell914 <ittikorn.tipson@gmail.com>
2026-09-03 11:25:39 +0700

    add validation for user creation

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,21 @@
-import express from "express";
-import { users } from "./fakeDB/fakeUsers.js";
-
-const app = express();
-
 // CRUD routes and endpoints
 
 // Read users
 app.get("/users", (req, res) => {
+  console.log(req);
   res.json(users);
 });
 
 // Create user
-app.post("/users", (req, res) => {});
+app.post("/users", (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.json({
+      error: "username, email and password are required!",
+    });
+  }
+});
 
 // Update user
 app.put("/users/:id", (req, res) => {});
