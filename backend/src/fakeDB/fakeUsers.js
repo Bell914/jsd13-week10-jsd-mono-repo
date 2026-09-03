c16d9b7b0b457b09ae6639eea09e9d220582ce8f
Bell914 <ittikorn.tipson@gmail.com>
2026-09-03 10:50:39 +0700

    setup backend with Express and project structure

---
+++ backend/src/fakeDB/fakeUsers.js
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    id: "1",
+    username: "Ittikorn",
+    email: "ittikorn@example.com",
+    password: "pass123",
+  },
+  {
+    id: "2",
+    username: "John",
+    email: "john@example.com",
+    password: "pass456",
+  },
+  {
+    id: "3",
+    username: "Jane",
+    email: "jane@example.com",
+    password: "pass789",
+  },
+];
